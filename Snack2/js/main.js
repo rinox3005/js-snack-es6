ebeda00c7724e5375c649b81c27ab7f5a062d00a
rinox3005 <peppe.rinox@gmail.com>
2024-05-16 18:06:42 +0200

    [UPD] snack 2 done

--- Snack2/js/main.js
+++ Snack2/js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// creo un array di oggetti
+const teams = [
+    { club: 'Milan', points: 0, fouls: 0 },
+    { club: 'Inter', points: 0, fouls: 0 },
+    { club: 'Juventus', points: 0, fouls: 0 },
+    { club: 'Ternana', points: 0, fouls: 0 },
+    { club: 'Palermo', points: 0, fouls: 0 },
+    { club: 'Atalanta', points: 0, fouls: 0 },
+    { club: 'Borussia Dortmund', points: 0, fouls: 0 }
+];
+
+// assegno ai valori delle chiavi punti e falli i numeri generati randomicamente
+Object.keys(teams).forEach((item) => {
+    teams[item].points = randomNumGen(1, 80);
+    teams[item].fouls = randomNumGen(1, 100);
+})
+// creo un nuovo array di oggetti che contengono soltanto le chiavi-valori club e fouls
+const newTeams = teams.map(({ club, fouls }) => ({ club, fouls }));
+
+// stampo in console le informazioni contenute nel mio nuovo array di oggetti
+newTeams.forEach((team) => {
+    console.log(`The club ${team.club} ended this season with ${team.fouls} total team fouls`);
+})
+
+//funzione generazione numero random
+function randomNumGen(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
