788188af98220469c1cf674ff7ca120afdd9208a
rinox3005 <peppe.rinox@gmail.com>
2024-05-16 17:11:20 +0200

    [UPD] snack 1 done

--- Snack1/js/main.js
+++ Snack1/js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// creo un array di oggetti
+const bikes = [
+    { model: 'Mountain Bike', weight: 17 },
+    { model: 'City Bike', weight: 12.7 },
+    { model: 'BMX Bike', weight: 27 },
+    { model: 'Canyon Spectral Bike', weight: 9.1 },
+    { model: 'Eleglide Bike', weight: 7.3 },
+    { model: 'Carbon Bike', weight: 21 },
+];
+
+// inserisco in un array tutti i valori dei pesi
+const weight = bikes.map(bikes => bikes.weight);
+
+// ordino i valori dell'array appena creato in ordine crescente 
+weight.sort(function (a, b) {
+    return a - b;
+});
+
+// prendo il valore in posizione 0 dell'array appena creato che corrisponderá al numero piú basso e quindi al peso minore
+const lighterBike = bikes.filter((bikes) => bikes.weight === weight[0]);
+
+// stampo in console l'oggetto che corrisponde alla bici o alle bici con il peso minore
+console.log(lighterBike);
+
+// ciclo dentro il nuovo array per stampare le informazioni al suo interno tramite template literal
+lighterBike.forEach((bike) => {
+    console.log(`The lightest bike is the ${[bike.model]} with a weight of ${bike.weight} kg`);
+})
